359f5c143c2f9a34696e31c62031e9ad1dafa6d7
Pinkicornio <carles.pascual.j@gmail.com>
2021-11-18 01:41:01 +0100

    Version 0.1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace youtube_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.rbVideo = new System.Windows.Forms.RadioButton();
+            this.rbAudio = new System.Windows.Forms.RadioButton();
+            this.download = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(88, 91);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(647, 20);
+            this.textBoxURL.TabIndex = 1;
+            // 
+            // rbVideo
+            // 
+            this.rbVideo.AutoSize = true;
+            this.rbVideo.Location = new System.Drawing.Point(378, 166);
+            this.rbVideo.Name = "rbVideo";
+            this.rbVideo.Size = new System.Drawing.Size(52, 17);
+            this.rbVideo.TabIndex = 6;
+            this.rbVideo.TabStop = true;
+            this.rbVideo.Text = "Video";
+            this.rbVideo.UseVisualStyleBackColor = true;
+            // 
+            // rbAudio
+            // 
+            this.rbAudio.AutoSize = true;
+            this.rbAudio.Location = new System.Drawing.Point(378, 143);
+            this.rbAudio.Name = "rbAudio";
+            this.rbAudio.Size = new System.Drawing.Size(52, 17);
+            this.rbAudio.TabIndex = 5;
+            this.rbAudio.TabStop = true;
+            this.rbAudio.Text = "Audio";
+            this.rbAudio.UseVisualStyleBackColor = true;
+            // 
+            // download
+            // 
+            this.download.Location = new System.Drawing.Point(344, 203);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(130, 45);
+            this.download.TabIndex = 4;
+            this.download.Text = "Descargar";
+            this.download.UseVisualStyleBackColor = true;
+            this.download.Click += new System.EventHandler(this.Download_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 310);
+            this.Controls.Add(this.rbVideo);
+            this.Controls.Add(this.rbAudio);
+            this.Controls.Add(this.download);
+            this.Controls.Add(this.textBoxURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Youtube - converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.RadioButton rbVideo;
+        private System.Windows.Forms.RadioButton rbAudio;
+        private System.Windows.Forms.Button download;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using AltoHttp;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace youtube_converter
+{
+    public partial class Form1 : Form
+    {
+        private HttpDownloader httpDownloader;
+        //Folder where music is saved
+        private const string FOLDERNAME = "videos_music";
+        //Wich disk is where the program is installed
+        private string root = Path.GetPathRoot(System.Reflection.Assembly.GetEntryAssembly().Location);
+        public Form1()
+        {
+            InitializeComponent();
+
+            root = root.Remove(root.Length - 1);
+            createYoutubefolder();
+            
+            if (!CheckYoutubedl())
+            {
+                DownloadYoutubedl();
+                MessageBox.Show("Descarga completada");
+            }
+            else
+            {
+                MessageBox.Show("Youtube-dl encontrado");
+            }
+        }
+
+        
+        private void createYoutubefolder()
+        {
+            string folderpath = $"{Application.StartupPath}\\{FOLDERNAME}";
+            if (!Directory.Exists(folderpath)) Directory.CreateDirectory(folderpath);
+        }
+
+        // Youtube-dl downloader
+        private void DownloadYoutubedl()
+        {
+            string youtubedl = "https://youtube-dl.org/downloads/latest/youtube-dl.exe";
+            httpDownloader = new HttpDownloader(youtubedl, $"{Application.StartupPath}\\{FOLDERNAME}\\{Path.GetFileName(youtubedl)}");
+            httpDownloader.Start();
+        }
+        private void Download_Click(object sender, EventArgs e)
+        {
+            string youtubedllocation = $"{Application.StartupPath}\\{FOLDERNAME}";
+            string fullpath = Path.GetFullPath(youtubedllocation);
+            
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.FileName = "cmd.exe";
+            ps.WindowStyle = ProcessWindowStyle.Normal;
+
+            if (rbAudio.Checked)
+            {
+                ps.Arguments = $@"/c cd\ && {root} && cd {fullpath} && youtube-dl -x --audio-format mp3 {textBoxURL.Text}";
+                Process.Start(ps);
+            }
+            else if (rbVideo.Checked)
+            {
+                ps.Arguments = $@"/c cd\ && {root} && cd {fullpath} && youtube-dl {textBoxURL.Text}";
+                Process.Start(ps);
+                //ps.Arguments = $"youtube"
+            }
+
+        }
+        private bool CheckYoutubedl()
+        {
+            string youtubedlname = "youtube-dl.exe";
+            string youtubedllocation = $"{Application.StartupPath}\\{FOLDERNAME}\\{youtubedlname}";
+
+            if (File.Exists(youtubedllocation)) return true;
+            return false;
+
+        }
+    }
+}
